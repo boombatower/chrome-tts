5eb08a2aa9f854477b2979d70f78d35b9d40eb64
boombatower <jimmy@boombatower.com>
2018-12-04 20:22:12 -0600

    Implement basic text chunking, error handling and stop when reading.

--- background.js
+++ background.js
@@ -1,7 +1,64 @@
+// Although the tts API indicates 32768 is the max the voice implementations are
+// inconsistent and somewhere over 300 start to break.
+const TTS_MAX_LENGTH = 300;
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    code: 'window.getSelection().toString();'
-  }, function(selectedText) {
-    chrome.tts.speak(selectedText[0], {'rate': 1.5});
+  chrome.tts.isSpeaking(function(speaking) {
+    if (speaking) {
+      chrome.tts.stop();
+    } else {
+      selectionRead();
+    }
   });
 });
+
+function selectionRead()
+{
+  chrome.tabs.executeScript({
+    code: 'window.getSelection().toString();'
+  }, function(result) {
+    if (result) {
+      textRead(result[0]);
+    } else {
+      console.log('no selection to read');
+    }
+  });
+}
+
+function textRead(text)
+{
+  var start = 0;
+  var end;
+  var substring;
+  do {
+    end = textChunk(text, start);
+    substring = text.substring(start, end);
+    start = end;
+
+    console.log(substring);
+    chrome.tts.speak(substring, {'rate': 1.5, enqueue: true, onEvent: ttsEvent});
+  }
+  while (start < text.length);
+}
+
+function textChunk(text, start)
+{
+  if (text.length - start > TTS_MAX_LENGTH) {
+    // Attempt to split by space.
+    for (var i = start + TTS_MAX_LENGTH - 1; i >= start; i--) {
+      if (text[i] == ' ') {
+        return i;
+      }
+    }
+
+    // Just break it since giant section.
+    return start + TTS_MAX_LENGTH;
+  }
+
+  return text.length;
+}
+
+function ttsEvent(event)
+{
+  console.log(event);
+}
